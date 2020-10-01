a7502480016d9d457d6c5e7ce6c233bb071ce9d9
unknown <crisjanng@gmail.com>
2020-10-01 16:09:34 +0800

    broken our homepage into smaller components, finished styling

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import MenuItem from '../menu-item/menu-item.component'
+
+// import '../directory/directory.styles.scss'
+import '../directory/directory.styles.scss'
+
+class Directory extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            section: [{
+                    title: 'HATS',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1
+                  },
+                  {
+                    title: 'JACKETS',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2
+                  },
+                  {
+                    title: 'SNEAKERS',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3
+                  },
+                  {
+                    title: 'WOMENS',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size: 'large',
+                    id: 4
+                  },
+                  {
+                    title: 'MENS',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'large',
+                    id: 5
+                  }]
+        }
+    }
+    render() {
+        return (
+            <div className='directory-menu'>
+                {this.state.section.map(({ title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory
---
+++ src/pages/homepage.component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../pages/homepage.style.scss'
+
+import Directory from '../components/directory/directory.component'
+
+const homepage = () => {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default homepage
